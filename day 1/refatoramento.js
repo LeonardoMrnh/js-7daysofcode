647a9b9ac7b63147ccd52332004b73574b4d55e7
leonardo <leomarinho1000@outlook.com>
2024-09-10 15:42:24 -0300

    first commit

---
+++ day 1/refatoramento.js
@@ -0,0 +1,29 @@
+let numeroUm = 1
+let stringUm = '1'
+let numeroDez = 10
+let stringDez = '10'
+let numeroTrinta = 30
+let stringTrinta = '30'
+
+
+//Comparação entre 1 e string de '1'
+if(numeroUm == stringUm){
+    console.log('As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes')
+} else {
+  console.log('As variáveis numeroUm e stringUm não tem o mesmo valor')
+}
+
+//Comparação entre 10 e string de '10'
+if (numeroDez == stringDez) {
+    console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes')
+  } else {
+    console.log('As variáveis numeroDez e stringDez não tem o mesmo valor')
+  }
+
+//Comparação entre 30 e string de '30'
+if (numeroTrinta == stringTrinta) {
+    console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes')
+  } else {
+    console.log('As variáveis numeroDez e stringDez não tem o mesmo valor')
+  }
+
